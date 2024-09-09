f7ad533e21eb4ddd82528d5144554e2fab860464
tracer-ysyx <tracer@ysyx.org>
2024-09-09 15:50:52 +0800

    > compile NEMU ysyx_22040000 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:50:52 up 19:41,  1 user,  load average: 0.59, 0.38, 0.32

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -45,9 +45,8 @@ static char* rl_gets() {
 static int cmd_si(char *args) {
 	int num;
 	char command[5];
-	char input[10]="si 1";
 
-	sscanf(input, "%s %d",command,&num);
+	sscanf("si 1", "%s %d",command,&num);
 	printf("%d",num);
 
 	return 0;
