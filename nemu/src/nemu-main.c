5c76727fec4849ba3d0f21e3dbc23875f97c98da
tracer-ysyx <tracer@ysyx.org>
2025-02-13 20:58:08 +0800

    > compile NEMU ysyx_22040000 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:58:08 up  4:33,  1 user,  load average: 0.46, 0.63, 0.55

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -15,6 +15,8 @@
 
 #include <common.h>
 
+#define LINE_MAX 1024
+
 void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
@@ -29,7 +31,64 @@ int main(int argc, char *argv[]) {
 #endif
 
   /* Start engine. */
-  engine_start();
+  //engine_start();
 
-  return is_exit_status_bad();
+  //return is_exit_status_bad();
+  int read_line(char *path);
+  read_line("input");
+
+
+}
+int read_line(char *path)
+{
+	FILE *fp;
+	int line_num = 0; // 文件行数
+	int line_len = 0; // 文件每行的长度
+	char buf[LINE_MAX] = {0}; // 行数据缓存
+
+	fp = fopen(path, "r");
+	if (NULL == fp) {
+		printf("open %s failed.\n", path);
+		return -1;
+	}
+
+	while(fgets(buf, LINE_MAX, fp)) {
+		line_num++;
+		line_len = strlen(buf);
+		// 排除换行符
+		if ('\n' == buf[line_len - 1]) {
+			buf[line_len - 1] = '\0';
+			line_len--;
+			if (0 == line_len) {
+				//空行
+				continue;
+			}
+		}
+		// windos文本排除回车符
+		if ('\r' == buf[line_len - 1]) {
+			buf[line_len - 1] = '\0';
+			line_len--;
+			if (0 == line_len) {
+				//空行
+				continue;
+			}
+		}
+		//printf("%s\n", buf);
+		/** 对每行数据(buf)进行处理 **/
+    word_t expr(char *e, bool *success);
+    uint32_t result_raw;
+    char arg[200] = {0};
+    sscanf(buf,"%d %s",&result_raw,arg);
+    printf("%d - %s\n",result_raw,arg);
+    bool success;
+    bool *ptr_success = &success;
+    uint32_t result;
+    result=expr(arg,ptr_success);
+    printf("%d\n",result);
+	}
+
+	
+	fclose(fp);
+
+	return line_num;
 }
