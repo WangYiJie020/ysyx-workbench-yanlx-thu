352d1a94ab4c0d5fe4a41c44294082aa328d4fe2
tracer-ysyx <tracer@ysyx.org>
2025-03-04 17:41:37 +0800

    > compile NEMU ysyx_22040000 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:41:37 up  1:57,  1 user,  load average: 0.24, 0.09, 0.04

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -39,7 +39,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   char reg1[3]={'x','\0'};
   //printf("%s\n",reg1);
   sscanf(s,"%s",reg1);
-  printf("%s\n",reg1);
+  //printf("%s\n",reg1);
   for(;i>0;i--) {
     if(strcmp(regs[32-i], reg1) == 0){
       flag=1;
@@ -49,7 +49,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
 	}
   if(!flag) {
     *success=0;
-    assert(0);
+    //assert(0);
   }
   return value;
 }
