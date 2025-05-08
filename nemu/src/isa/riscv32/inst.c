cd564337fc797f4a4512ffffcf3e90f10320d244
tracer-ysyx <tracer@ysyx.org>
2025-05-08 22:42:34 +0800

    > compile NEMU ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:42:34 up 2 days,  8:29,  5 users,  load average: 0.33, 0.34, 0.41

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -36,6 +36,10 @@ enum {
                             | (BITS(i, 30, 21) << 1) \
                             | (BITS(i, 20, 20) << 11) \
                             | (BITS(i, 19, 12) << 12); } while(0)
+#define immSB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) \
+                            | (BITS(i, 30, 25) << 5) \
+                            | (BITS(i, 11, 8) << 1) \
+                            | (BITS(i, 7, 7) << 11); } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -43,10 +47,12 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   int rs2 = BITS(i, 24, 20);
   *rd     = BITS(i, 11, 7);
   switch (type) {
+    case TYPE_R: src1R(); src2R();         break;
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_UJ:                  immUJ(); break;
+    case TYPE_SB:src1R(); src2R(); immSB(); break;
   }
 }
 
@@ -70,7 +76,13 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2););
   INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, s->dnpc = src1 + imm;R(rd)=s->pc + 4;);
   INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    ,UJ, s->dnpc = s->pc + imm;R(rd)=s->pc + 4;);
-  //INSTPAT("??????? ????? ????? 010 ????? 01000 11", beq    ,SB, );
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    ,SB, \
+          if(src1 == src2){s->dnpc = s->pc + imm;});
+  INSTPAT("??????? ????? ????? 001 ????? 11000 11", bne    ,SB, \
+          if(src1 != src2){s->dnpc = s->pc + imm;});
+  INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw     , I, R(rd) = Mr(src1 + imm, 4););
+  INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add    , R, R(rd) = src1 + src2;);
+  INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(rd) = src1 - src2;);
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
