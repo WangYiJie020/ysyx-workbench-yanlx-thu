aa956270066e02b77b106bf098d769fece2237a4
tracer-ysyx <tracer@ysyx.org>
2025-05-09 09:54:24 +0800

    > compile NEMU ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:54:24 up 2 days, 19:41,  5 users,  load average: 2.00, 1.44, 1.21

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,7 +23,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S, TYPE_UJ, TYPE_SB, TYPE_R,
+  TYPE_I, TYPE_U, TYPE_S, TYPE_UJ, TYPE_SB, TYPE_R, TYPE_SRAI,
   TYPE_N, // none
 };
 
@@ -40,6 +40,7 @@ enum {
                             | (BITS(i, 30, 25) << 5) \
                             | (BITS(i, 11, 8) << 1) \
                             | (BITS(i, 7, 7) << 11); } while(0)
+#define immSRAI() do { *imm = SEXT(BITS(i, 24, 20), 5); } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -53,6 +54,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_UJ:                  immUJ(); break;
     case TYPE_SB:src1R(); src2R(); immSB(); break;
+    case TYPE_SRAI: src1R();       immSRAI(); break;
   }
 }
 
@@ -84,6 +86,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 011 ????? 00100 11", sltiu  , I, \
             if(src1 < imm) R(rd) = 1;\
             else R(rd) = 0;); 
+  INSTPAT("0100000 ????? ????? 101 ????? 00100 11", srai   , SRAI, R(rd) = src1 >> ((sword_t)imm););
   INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add    , R, R(rd) = src1 + src2;);
   INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(rd) = src1 - src2;);
 
