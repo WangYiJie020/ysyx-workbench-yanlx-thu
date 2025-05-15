2f9e3d0dda99ddf81e58e5d0fbeffdd30db0f590
tracer-ysyx <tracer@ysyx.org>
2025-05-15 17:00:25 +0800

    > compile NEMU ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:00:25 up 19:58,  1 user,  load average: 0.65, 0.65, 0.55

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,14 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  assert(dst != NULL && src != NULL); //检查指针的有效性  
+
+  char *ret = dst;  //记下dst的初始地址防止找不到
+
+  while ((*dst++=*src++)!='\0'); //将src中所有字符(包括'\0')一个字符一个字符的拷贝到dst(包括'\0')。在赋值完'\0'后，循环停止
+
+  return ret;
+
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,10 +24,27 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *tmp = dst;//记下dest的初始地址防止找不到
+ 
+  while (*dst)
+          dst++; //找到dest的末尾
+  while ((*dst++ = *src++) != '\0'); //将src附加到dest后面
+
+  return tmp;
+
 }
 
 int strcmp(const char *s1, const char *s2) {
+  if (s1 == NULL || s2 == NULL) return 0;
+  int ret = 0 ;
+  while( !(ret = *(unsigned char *)s1 - *(unsigned char *)s2) && *s2)
+      ++s1, ++s2;
+  if ( ret < 0 )
+      ret = -1 ;
+  else if ( ret > 0 )
+      ret = 1 ;
+  return( ret );
+
   panic("Not implemented");
 }
 
@@ -29,7 +53,17 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  const unsigned char uc = c;
+
+    unsigned char *su;
+
+ 
+
+    for(su = s; 0 < n; ++su, --n)
+
+        *su = uc;
+
+    return (s);
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -37,11 +71,20 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+ panic("Not implemented");
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char *su1, *su2;
+
+  for(su1 = s1, su2 = s2; 0 < n; ++su1, ++su2, --n)
+
+      if(*su1 != *su2)
+
+          return ((*su1 < *su2) ? -1 : +1);
+
+  return 0;
+
 }
 
 #endif
