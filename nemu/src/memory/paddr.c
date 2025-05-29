7bf4725d1d2cc16415918b7379a4cecd1b1d7b58
tracer-ysyx <tracer@ysyx.org>
2025-05-29 08:44:13 +0800

    > compile NEMU ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:44:13 up 24 min,  1 user,  load average: 0.80, 0.65, 0.43

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -29,11 +29,17 @@ paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
+  #ifdef CONFIG_MTRACE
+  Log("read addr:%x, len:%d\n",addr,len);
+  #endif
   return ret;
 }
 
 static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
+  #ifdef CONFIG_MTRACE
+  Log("write addr:%x, len:%d, data:%x\n",addr,len,data);
+  #endif
 }
 
 static void out_of_bound(paddr_t addr) {
