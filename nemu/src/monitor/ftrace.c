8e4929258c88ff3332b77bb43415fe6ba86127d2
tracer-ysyx <tracer@ysyx.org>
2025-06-06 13:44:52 +0800

    > compile NEMU ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:44:52 up  4:52,  1 user,  load average: 0.79, 0.86, 0.82

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -140,7 +140,7 @@ void trace_call(paddr_t pc, paddr_t target){
 
     int i = find_symbol_func(target,true);
     //Log(FMT_PADDR ":%*scall [%s@" FMT_PADDR "]\n",pc,(call_depth-3)*2,i>=0?symbol_tbl[i].name:"???",target);
-    Log("%s",i>=0?symbol_tbl[i].name:"???");
+    Log("%s\n",i>=0?symbol_tbl[i].name:"???");
 }
 
 void trace_ret(paddr_t pc) {
@@ -151,7 +151,7 @@ void trace_ret(paddr_t pc) {
     int i = find_symbol_func(pc,false);
 
     //Log(FMT_PADDR ":%*sret [%s]\n",pc,(call_depth-3)*2,i>=0?symbol_tbl[i].name:"???",);
-    Log("%s",i>=0?symbol_tbl[i].name:"???");
+    Log("%s\n",i>=0?symbol_tbl[i].name:"???");
 
     call_depth--;
 
