724d91ac1f0d489511801b3deef5badd24798afa
tracer-ysyx <tracer@ysyx.org>
2025-06-06 17:09:07 +0800

    > compile NEMU ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:09:07 up  8:16,  1 user,  load average: 0.64, 0.63, 0.61

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -105,7 +105,7 @@ void parse_elf(const char * elf_file){
             symbol_tbl[i].addr = symbol.st_value;
             symbol_tbl[i].info = symbol.st_info;
             symbol_tbl[i].size = symbol.st_size;
-            Log("%s,%x,%x,%d",symbol_tbl[i].name,symbol_tbl[i].addr,symbol_tbl[i].info,(int)symbol_tbl[i].size);
+            printf("%s,%x,%x,%d",symbol_tbl[i].name,symbol_tbl[i].addr,symbol_tbl[i].info,(int)symbol_tbl[i].size);
         }
         symbol_tbl_size = num_symbols;
     }
@@ -150,7 +150,7 @@ void trace_call(paddr_t pc, paddr_t target){
     int i = find_symbol_func(target,true);
     //Log(FMT_PADDR ":%*scall [%s@" FMT_PADDR "]\n",pc,(call_depth-3)*2,i>=0?symbol_tbl[i].name:"???",target);
     Log("%s\n",i>=0?symbol_tbl[i].name:"???");
-    printf("call\n");
+    //printf("call\n");
 }
 
 void trace_ret(paddr_t pc) {
