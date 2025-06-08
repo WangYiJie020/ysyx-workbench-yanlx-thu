02f8adc20164ae1c63a79f19788c055248d3d5d0
tracer-ysyx <tracer@ysyx.org>
2025-06-08 18:42:13 +0800

    > compile NEMU ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:42:13 up 42 min,  1 user,  load average: 1.07, 1.06, 0.83

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -132,7 +132,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000000 ????? ????? 001 ????? 01100 11", sll    , R, R(rd) = src1 << src2;);
   INSTPAT("0000000 ????? ????? 010 ????? 01100 11", slt    , R, \
             if((sword_t)src1 < (sword_t)src2) R(rd) = 1;\
-            else R(rd) = 1;); 
+            else R(rd) = 0;); 
   INSTPAT("0000000 ????? ????? 011 ????? 01100 11", sltu   , R, \
             if(src1 < src2) R(rd) = 1;\
             else R(rd) = 0;); 
