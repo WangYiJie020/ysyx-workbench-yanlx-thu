960879278e50d84bcac521194f18f95c20845f17
tracer-ysyx <tracer@ysyx.org>
2025-06-11 20:33:18 +0800

    > compile NEMU ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:33:18 up 10:57,  1 user,  load average: 0.27, 0.47, 0.59

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -83,6 +83,7 @@ static int parse_args(int argc, char *argv[]) {
   };
   int o;
   while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
+    printf("%c,%d/n",o,o);
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
