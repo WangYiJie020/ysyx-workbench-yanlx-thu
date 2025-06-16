2e2334af9cd5f8266980b38af47d821890daab37
tracer-ysyx <tracer@ysyx.org>
2025-06-16 18:01:24 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:01:24 up 2 days,  4:04,  1 user,  load average: 1.06, 0.93, 0.99

--- npc/vsrc/regfile.v
+++ npc/vsrc/regfile.v
@@ -1,4 +1,4 @@
-import "DPI-C" function void reg_return_value(logic[31:0] regvalue);
+import "DPI-C" function void reg_return_value(logic[31:0] regvalue[32]);
 
 module regfile #(ADDR_WIDTH = 5, DATA_WIDTH = 32) (
   input clk,
