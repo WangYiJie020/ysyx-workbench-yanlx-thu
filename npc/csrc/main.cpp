4e303b140486ce610cd18de6568e059ecc9e39e5
tracer-ysyx <tracer@ysyx.org>
2025-06-16 19:30:45 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:30:45 up 2 days,  5:33,  1 user,  load average: 0.56, 0.65, 0.64

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -14,7 +14,8 @@
 #define ARRLEN(arr) (int)(sizeof(arr) / sizeof(arr[0]))
 #define word_t uint32_t 
 
-int mem[4294967295];
+//int mem[4294967295];
+int mem[10000];
 
 
 int cpu_state = 0;
@@ -50,7 +51,7 @@ WP* new_wp(char * arg, uint32_t value);
 void free_wp(int num);
 
 int pmem_read(int pc) {
-  return mem[(pc)/4];
+  return mem[(pc-0x80000000)/4];
 
 }
 
@@ -133,7 +134,7 @@ static long load_img() {
   printf("The image is %s, size = %ld\n", img_file, size);
 
   fseek(fp, 0, SEEK_SET);
-  int ret = fread(mem+0x20000000, size, 1, fp);
+  int ret = fread(mem, size, 1, fp);
   assert(ret == 1);
 
   fclose(fp);
@@ -808,7 +809,7 @@ void cpu_exec(int num) {
       printf("finish\n");
       break;
     }
-    //top->inst = pmem_read(top->pc);
+    top->inst = pmem_read(top->pc);
     top->clk = 0; top->eval();
     top->clk = 1; top->eval();
     //printf("pc=%x\n",top->pc);
