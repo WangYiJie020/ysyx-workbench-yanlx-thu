8bffcff4b4ca53f3408ac6ba1499cfb81d417e0a
tracer-ysyx <tracer@ysyx.org>
2025-06-16 19:24:20 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:24:20 up 2 days,  5:27,  1 user,  load average: 0.65, 0.62, 0.62

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -133,7 +133,7 @@ static long load_img() {
   printf("The image is %s, size = %ld\n", img_file, size);
 
   fseek(fp, 0, SEEK_SET);
-  int ret = fread(mem+0x80000000, size, 1, fp);
+  int ret = fread(mem+0x20000000, size, 1, fp);
   assert(ret == 1);
 
   fclose(fp);
