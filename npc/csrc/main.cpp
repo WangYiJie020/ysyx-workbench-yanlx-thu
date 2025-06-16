271151abe60f322a3175ea148638be56cdcd0d0a
tracer-ysyx <tracer@ysyx.org>
2025-06-16 15:59:00 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:59:00 up 2 days,  2:02,  1 user,  load average: 0.97, 0.57, 0.42

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <assert.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include "Vtop.h"
 #include "verilated.h"
 
@@ -74,20 +76,223 @@ static int parse_args(int argc, char *argv[]) {
   return 0;
 }
 
+/* We use the `readline' library to provide more flexibility to read from stdin. */
+static char* rl_gets() {
+  static char *line_read = NULL;
+
+  if (line_read) {
+    free(line_read);
+    line_read = NULL;
+  }
+
+  line_read = readline("(nemu) ");
+
+  if (line_read && *line_read) {
+    add_history(line_read);
+  }
+
+  return line_read;
+}
+
+static int cmd_si(char *args) {
+	char *arg = strtok(NULL, " ");
+	char num_c[5];
+	int num;
+	if (arg == NULL) {
+		num=1;
+	}
+	else {
+		strcpy( num_c, arg);
+		sscanf(num_c,"%d",&num);
+		//printf("%d",num);
+	}
+	cpu_exec(num);
+
+
+	return 0;
+}
+
+static int cmd_info(char *args) {
+	char *arg = strtok(NULL, " ");
+       
+        if (arg == NULL) {
+                printf("please add sub command and try again\n");
+        }
+        else {
+		if(strcmp(arg,"r")==0){
+			isa_reg_display();
+
+		}
+		else if(strcmp(arg,"w")==0){
+      print_wp();
+			//printf("www");
+		}
+		else{
+			printf("Unknown command '%s'\n", arg);
+		}
+	}
+        
+        return 0;
+}
+
+static int cmd_x(char *args) {
+  char *arg = strtok(NULL, " ");
+	unsigned int num;
+	char line[100];
+
+  if (arg == NULL) {
+          printf("please add sub command and try again\n");
+  }
+  else {
+    strcpy( line, arg);
+    //printf("%s\n",line);
+    sscanf(line,"%d",&num);
+    arg = strtok(NULL, " ");
+    if(arg==NULL) {
+      printf("please add sub command and try again\n");
+    }
+    else {
+      strcpy( line, arg);
+      bool success;
+      bool *ptr_success = &success;
+      uint32_t result;
+      //printf("%s\n",arg);
+      result=expr(line,ptr_success);
+      //printf("%s\n",line);
+      //sscanf(result,"%x",&addr);
+      //printf("%d %x",num,place);
+      int i,j;
+      for(i=0;i<num;i++) {
+        printf("0x%8x: ",result);
+        for(j=3;j>=0;j--) {
+          printf("%02x ",vaddr_read(result+j,1));
+        }
+        result+=4;
+        printf("\n");
+        
+      }
+      
+    }
+
+                
+	}
+	return 0;
+}
+
+static int cmd_p(char *args) {
+	char *arg = strtok(NULL, "\0");
+	bool success;
+	bool *ptr_success = &success;
+	uint32_t result;
+	//printf("%s\n",arg);
+	result=expr(arg,ptr_success);
+	printf("result=%u\n",result);
+        return 0;
+}
+
+static int cmd_w(char *args) {
+  char *arg = strtok(NULL, "\0");
+	bool success;
+	bool *ptr_success = &success;
+	uint32_t result;
+	//printf("%s\n",arg);
+	result=expr(arg,ptr_success);
+  //printf("result=%u\n",result);
+  new_wp(arg,result);
+        return 0;
+}
+
+static int cmd_d(char *args) {
+  char *arg = strtok(NULL, " ");
+	char num_c[5];
+	int num;
+	if (arg == NULL) {
+		assert(0);
+	}
+	else {
+		strcpy( num_c, arg);
+		sscanf(num_c,"%d",&num);
+		//printf("%d",num);
+	}
+  free_wp(num);
+        return 0;
+}
+
+
+static int cmd_c(char *args) {
+  cpu_exec(-1);
+  return 0;
+}
+
+int npc_state=1;
+
+static int cmd_q(char *args) {
+  //exit(0);
+  npc_state = 0;
+  return -1;
+}
+
+static int cmd_help(char *args);
+
+static struct {
+  const char *name;
+  const char *description;
+  int (*handler) (char *);
+} cmd_table [] = {
+  { "help", "Display information about all supported commands", cmd_help },
+  { "c", "Continue the execution of the program", cmd_c },
+  { "q", "Exit NEMU", cmd_q },
+  { "si", "Continue the execution for N steps, N is number you input, default N=1", cmd_si },
+  { "info", "print the state of the program", cmd_info },
+  { "x", "scan the memory", cmd_x },
+  { "p", "", cmd_p },
+  { "w", "", cmd_w },
+  { "d", "", cmd_d }, 
+
+  /* TODO: Add more commands */
+
+};
+
+#define NR_CMD ARRLEN(cmd_table)
+
+static int cmd_help(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  int i;
+
+  if (arg == NULL) {
+    /* no argument given */
+    for (i = 0; i < NR_CMD; i ++) {
+      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+    }
+  }
+  else {
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(arg, cmd_table[i].name) == 0) {
+        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        return 0;
+      }
+    }
+    printf("Unknown command '%s'\n", arg);
+  }
+  return 0;
+}
+
+VerilatedContext* contextp = new VerilatedContext;
+Vtop* top = new Vtop{contextp};
+VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
+
 int main(int argc, char** argv) {
   parse_args(argc, argv);
   long img_size = load_img();
-  VerilatedContext* contextp = new VerilatedContext;
+  
   contextp->commandArgs(argc, argv);
-  Vtop* top = new Vtop{contextp};
-
-  VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
+  
   contextp->traceEverOn(true); //打开追踪功能
   top->trace(tfp, 0); //
   tfp->open("wave.vcd"); //设置输出的文件wave.vcd
 
-  void single_cycle();
-  void reset(int n);
+  init_sdb();
 
   int n = 10;
   top->rst_n = 0;
@@ -100,7 +305,31 @@ int main(int argc, char** argv) {
   }
   top->rst_n = 1;
 
-  while (!contextp->gotFinish()) {
+  while (!contextp->gotFinish() || npc_state==0) {
+    for (char *str; (str = rl_gets()) != NULL; ) {
+      char *str_end = str + strlen(str);
+
+      /* extract the first token as the command */
+      char *cmd = strtok(str, " ");
+      if (cmd == NULL) { continue; }
+
+      /* treat the remaining string as the arguments,
+      * which may need further parsing
+      */
+      char *args = cmd + strlen(cmd) + 1;
+      if (args >= str_end) {
+        args = NULL;
+      }
+      int i;
+      for (i = 0; i < NR_CMD; i ++) {
+        if (strcmp(cmd, cmd_table[i].name) == 0) {
+          if (cmd_table[i].handler(args) < 0) { return; }
+          break;
+        }
+      }
+
+      if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+    }
     top->inst = pmem_read(top->pc);
     top->clk = 0; top->eval();
     top->clk = 1; top->eval();
