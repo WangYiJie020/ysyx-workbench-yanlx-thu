7ee6ef0bb6eeb7619d09217f6329a29e65246d77
tracer-ysyx <tracer@ysyx.org>
2025-06-17 20:08:47 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:08:47 up 3 days,  6:11,  1 user,  load average: 0.76, 0.61, 0.52

--- npc/obj_dir/Vtop.mk
+++ npc/obj_dir/Vtop.mk
@@ -43,6 +43,7 @@ VM_USER_LDLIBS = \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
+	log \
 	main \
 
 # User .cpp directories (from .cpp's on Verilator command line)
@@ -59,6 +60,8 @@ include $(VERILATOR_ROOT)/include/verilated.mk
 ### Executable rules... (from --exe)
 VPATH += $(VM_USER_DIR)
 
+log.o: /home/yanlx/ysyx-workbench/npc/csrc/log.c
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 main.o: /home/yanlx/ysyx-workbench/npc/csrc/main.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 
