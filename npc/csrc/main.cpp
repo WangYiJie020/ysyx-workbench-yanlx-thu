9211a624140734318b503d5be6c8443791d64598
tracer-ysyx <tracer@ysyx.org>
2025-06-17 21:48:42 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:48:42 up 3 days,  7:51,  1 user,  load average: 0.56, 0.71, 0.55

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -43,13 +43,6 @@ extern WP *head;
 
 #define  DIFFTEST_ON
 
-extern regfile dut_reg;
-
-enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF};
-void (*ref_difftest_memcpy)(uint64_t addr, void *buf, size_t n, bool direction) = NULL;
-void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
-void (*ref_difftest_exec)(uint64_t n) = NULL;
-void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
 word_t expr(char *e, bool *success);
 WP* new_wp();
@@ -128,51 +121,6 @@ extern "C" void reg_return_value(uint32_t regvalue[32]) {
     reg_value[i] = regvalue[i];
   }
   
-}
-
-void difftest_init(char *ref_so_file, long img_size) {
-  assert(ref_so_file != NULL);
-
-  void *handle;
-  handle = dlopen(ref_so_file, RTLD_LAZY);
-  assert(handle);
-
-  ref_difftest_memcpy = (void (*)(uint64_t addr, void *buf, size_t n, bool direction))dlsym(handle , "difftest_memcpy");
-  assert(ref_difftest_memcpy);
-
-  ref_difftest_regcpy = (void (*)(void *dut, bool direction))dlsym(handle, "difftest_regcpy");
-  assert(ref_difftest_regcpy);
-
-  ref_difftest_exec = (void (*)(uint64_t n))dlsym(handle, "difftest_exec");
-  assert(ref_difftest_exec);
-
-  ref_difftest_raise_intr = (void (*)(uint64_t NO))dlsym(handle, "difftest_raise_intr");
-  assert(ref_difftest_raise_intr);
-
-  void (*ref_difftest_init)() = (void (*)())dlsym(handle, "difftest_init");
-  assert(ref_difftest_init);
-
-  ref_difftest_init();
-
-  ref_difftest_memcpy(PMEM_START,guest_to_host(PMEM_START), img_size, DIFFTEST_TO_REF);
-
-  dut_reg.pc = INST_START;
-  ref_difftest_regcpy(&dut_reg, DIFFTEST_TO_REF);
-
-}
-
-bool difftest_check() {
-  regfile ref;
-  ref_difftest_regcpy(&ref, DIFFTEST_TO_DUT);
-  return checkregs(&ref, &dut_reg);
-}
-
-void difftest_step() {
-  ref_difftest_exec(1);
-}
-
-void diff_cpdutreg2ref() {
-  ref_difftest_regcpy(&dut_reg, DIFFTEST_TO_REF);
 }
 
 
@@ -976,7 +924,7 @@ int main(int argc, char** argv) {
   long img_size = load_img();
 
   init_log("npc-log.txt");
-  init_difftest(diff_so_file, img_size);
+  //init_difftest(diff_so_file, img_size);
   init_sdb();
   
   contextp->commandArgs(argc, argv);
