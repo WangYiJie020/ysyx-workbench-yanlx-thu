205df40c336d97e2161f0eca41c65475cc126398
tracer-ysyx <tracer@ysyx.org>
2025-06-17 20:13:43 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:13:43 up 3 days,  6:16,  1 user,  load average: 0.67, 0.71, 0.59

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -814,7 +814,22 @@ void print_wp(){
   }
 }
 
+extern uint64_t g_nr_guest_inst;
 
+FILE *log_fp = NULL;
+
+
+void init_log(const char *log_file) {
+  log_fp = stdout;
+  if (log_file != NULL) {
+    FILE *fp = fopen(log_file, "w");
+    if(fp == NULL)
+      printf("Can not open '%s'", log_file);
+    log_fp = fp;
+  }
+  Log("Log is written to %s", log_file ? log_file : "stdout");
+  //printf("Log is written to %s\n", log_file ? log_file : "stdout");
+}
 
 
 static void trace_and_difftest() {
