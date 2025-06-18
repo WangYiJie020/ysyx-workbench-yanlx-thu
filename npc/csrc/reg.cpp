5d79e0e1dd184245ef5c42632221e61510efac99
tracer-ysyx <tracer@ysyx.org>
2025-06-18 14:25:09 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:25:09 up 4 days, 28 min,  1 user,  load average: 0.82, 0.73, 0.66

--- npc/csrc/reg.cpp
+++ npc/csrc/reg.cpp
@@ -1,3 +1,10 @@
+#include "common.h"
+#include "debug.h"
+#include "macro.h"
+#include "utils.h"
+#include "include.h"
+
+
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
