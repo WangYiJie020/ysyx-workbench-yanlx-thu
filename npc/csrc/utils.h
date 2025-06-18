e81ad03acbaba3b769807a7e992c354552978024
tracer-ysyx <tracer@ysyx.org>
2025-06-18 15:00:14 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:00:14 up 4 days,  1:03,  1 user,  load average: 0.48, 0.55, 0.61

--- npc/csrc/utils.h
+++ npc/csrc/utils.h
@@ -60,11 +60,8 @@ uint64_t get_time();
 #define log_write(...) IFDEF(CONFIG_TARGET_NATIVE_ELF, \
   do { \
     extern FILE* log_fp; \
-    extern bool log_enable(); \
-    if (log_enable()) { \
       fprintf(log_fp, __VA_ARGS__); \
       fflush(log_fp); \
-    } \
   } while (0) \
 )
 
