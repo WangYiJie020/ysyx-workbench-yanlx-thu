ec79b7f2e5240a5694550b537833f47434918816
tracer-ysyx <tracer@ysyx.org>
2025-06-18 13:41:30 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:41:30 up 3 days, 23:44,  1 user,  load average: 0.92, 0.84, 0.55

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -24,7 +24,7 @@
 
 
 
-int mem[0xffffffff];
+extern int mem[0xffffffff];
 
 int cpu_state = 0;
 
@@ -32,16 +32,7 @@ VerilatedContext* contextp = new VerilatedContext;
 Vtop* top = new Vtop{contextp};
 VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
 
-typedef struct watchpoint {
-  int NO;
-  char tokens[100];
-  uint32_t value;
-  struct watchpoint *next;
 
-  /* TODO: Add more members if necessary */
-
-} WP;
-extern WP *head;
 
 typedef struct {
   uint32_t gpr[32];
@@ -765,108 +756,7 @@ static int cmd_help(char *args) {
   return 0;
 }
 
-#define NR_WP 32
 
-static WP wp_pool[NR_WP] = {};//pool
-WP *head = NULL,*free_ = NULL;
-
-void init_wp_pool() {
-  int i;
-  for (i = 0; i < NR_WP; i ++) {
-    wp_pool[i].NO = i;
-    strcpy(wp_pool[i].tokens,"\0\0\0\0\0\0\0\0\0\0\0\0");
-    wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
-  }
-
-  head = NULL;
-  free_ = wp_pool;
-}
-
-/* TODO: Implement the functionality of watchpoint */
-
-WP* new_wp(char * arg, uint32_t value){
-  WP* tmp=NULL;
-  if(free_ == NULL) {
-    assert(0);
-  }
-  else {
-    
-    if(head==NULL) {
-      head = free_;
-      free_ = free_->next;
-      head->next=NULL;
-    }
-    else {
-      tmp = free_;
-      free_ = free_->next;
-      tmp->next = head;
-      head = tmp;
-    }
-
-    strcpy(head->tokens,arg);
-    printf("%s",head->tokens);
-    head->value = value;
-
-    
-  }
-  return head;
-
-}
-
-
-
-void free_wp(int num){
-  WP *p,*q;
-  p = head;
-  int flag=0;
-  if(head == NULL) {
-    assert(0);
-  }
-  else {
-    if(p->NO == num){
-      head = head->next;
-      p->next = free_;
-      free_ = p;
-      //printf("wp==head\n\r");
-    }
-    else {
-      p=head;
-      q=head->next;
-
-      while(q!=NULL) { 
-        if(q->NO==num){
-          p->next = q->next;
-          q->next=free_;
-          free_ = q;
-          flag=1;
-          break;
-        }
-        p=p->next;
-        q=q->next;
-      }
-      if(flag==0) printf("no watchpoint numbered %d\n\r",num);
-
-    }
-    
-  }
-  return;
-
-}
-
-void print_wp(){
-  WP *p;
-  p = head;
-  if(p==NULL) {
-    printf("no watchpoint!\n\r");
-  }
-  else {
-    while(p!=NULL) {
-      printf("watchpoint NO.%d %s 0x%x\n\r",p->NO,p->tokens,p->value);
-      p = p->next;
-    }
-    
-  }
-}
 
 extern uint64_t g_nr_guest_inst;
 
