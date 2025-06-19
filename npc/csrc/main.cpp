3911c0503c70ca954e57b119897d5bcd660336e0
tracer-ysyx <tracer@ysyx.org>
2025-06-19 16:26:35 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:26:35 up 5 days,  2:29,  1 user,  load average: 0.36, 0.60, 0.75

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -269,15 +269,14 @@ void cpu_exec(int num) {
       printf("abort! at pc=%x\n",top->pc);
       break;
     }
-    
-    
+    top->inst = pmem_read(top->pc);
     top->clk = 0; top->eval();
     top->clk = 1; top->eval();
-    top->inst = pmem_read(top->pc);
+    trace_and_difftest();
     //printf("pc=%x\n",top->pc);
     tfp->dump(contextp->time()); //dump wave
     contextp->timeInc(1); //推动仿真时间
-    trace_and_difftest();
+    
   }
 }
 
