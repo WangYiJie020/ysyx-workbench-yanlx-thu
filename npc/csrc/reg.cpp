62567ebd7b06a6b0748cc3790856e19c6188a7b3
tracer-ysyx <tracer@ysyx.org>
2025-06-19 15:40:52 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:40:52 up 5 days,  1:43,  1 user,  load average: 0.51, 0.38, 0.32

--- npc/csrc/reg.cpp
+++ npc/csrc/reg.cpp
@@ -47,19 +47,20 @@ word_t isa_reg_str2val(const char *s, bool *success) {
 }
 
 bool checkregs(regfile *ref, regfile *dut) {
+    bool check = true;
   if(ref->pc != dut->pc){
     printf("difftest error: ");
     printf("next reg pc is diff: ref = 0x%x, dut = 0x%x\n",ref->pc,dut->pc);
-    return false;
+    check = false;
   }
   for (int i = 0; i < ARRLEN(regs); i++) {
     if(ref->gpr[i] != dut->gpr[i]){
       printf("difftest error at nextpc = 0x%x, ",dut->pc);
       printf("reg %s is diff: ref = 0x%x, dut = 0x%x\n",regs[i],ref->gpr[i],dut->gpr[i]);
-      return false;
+      check = false;
     }
   }
-  return true;
+  return check;
 }
 
 void print_regs(){
