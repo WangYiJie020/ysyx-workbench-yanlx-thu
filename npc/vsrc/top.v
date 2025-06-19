43cf485fcc7a9ca30d0d4c21d3833c50ca28d11a
tracer-ysyx <tracer@ysyx.org>
2025-06-19 16:21:25 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:21:25 up 5 days,  2:24,  1 user,  load average: 0.50, 0.74, 0.82

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -151,15 +151,19 @@ module top(
   );
 
   always@(*) begin
+    
+    if(inst == 32'h00100073) begin
+      ebreak();
+      $finish;
+    end
+  end
+
+  always(*) begin
     reg_return_value(reg_file[0],reg_file[1],reg_file[2],reg_file[3],reg_file[4],reg_file[5],reg_file[6],
       reg_file[7],reg_file[8],reg_file[9],reg_file[10],reg_file[11],reg_file[12],reg_file[13],reg_file[14],
       reg_file[15],reg_file[16],reg_file[17],reg_file[18],reg_file[19],reg_file[20],reg_file[21],reg_file[22],
       reg_file[23],reg_file[24],reg_file[25],reg_file[26],reg_file[27],reg_file[28],reg_file[29],reg_file[30],
       reg_file[31],pc);
-    if(inst == 32'h00100073) begin
-      ebreak();
-      $finish;
-    end
   end
 
 endmodule
