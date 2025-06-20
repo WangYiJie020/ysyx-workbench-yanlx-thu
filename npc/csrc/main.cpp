cfed14b8586ae63736850ec75399a281ff173246
tracer-ysyx <tracer@ysyx.org>
2025-06-20 17:48:33 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:48:33 up 6 days,  3:51,  1 user,  load average: 0.66, 0.34, 0.32

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -43,8 +43,10 @@ void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
 extern "C" int pmem_read(int raddr) {
   // 总是读取地址为`raddr & ~0x3u`的4字节返回
+  
+  uint32_t tmp = (uint32_t)raddr /4; //int类型是有符号的，要转成无符号的
   printf("raddr = %x,data= %x\n",raddr,mem[raddr/4]);
-  return mem[raddr / 4];
+  return mem[tmp];
 }
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
   // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
