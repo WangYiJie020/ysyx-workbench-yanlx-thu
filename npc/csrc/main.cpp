dbebf29212e58d2efbab5a5ebe30a70acf4d4f2f
tracer-ysyx <tracer@ysyx.org>
2025-06-20 19:44:48 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:44:48 up 6 days,  5:47,  1 user,  load average: 1.16, 0.56, 0.37

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -299,7 +299,7 @@ int main(int argc, char** argv) {
   parse_args(argc, argv);
   long img_size = load_img();
 
-  init_log("npc-log.txt");
+  //init_log("npc-log.txt");
   //init_difftest(diff_so_file, img_size, difftest_port);
   //init_sdb();
   cpu_state = NPC_RUNNING;
