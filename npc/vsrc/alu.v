9aa273dc974397bf50e8fbcbaf095e016d72b972
tracer-ysyx <tracer@ysyx.org>
2025-06-24 19:35:11 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:35:11 up 2 days, 22:01,  1 user,  load average: 0.54, 0.51, 0.43

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -21,7 +21,7 @@ always@(*) begin
         4'b0101: alu_result = a - b;
         4'b0110: alu_result = a << b;
         4'b0111: alu_result = a >> b;
-        4'b1000: alu_result = $signed(a) >> $signed(b);
+        4'b1000: alu_result = ($signed(a)) >> ($signed(b));
         4'b1001: alu_result = (a < b) ? 32'd1 : 32'd0;
         4'b1010: alu_result = ($signed(a) < $signed(b)) ? 32'd1 : 32'd0;
         //4'b1011: alu_result = mux_tmp[31:0];
