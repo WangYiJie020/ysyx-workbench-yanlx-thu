623a1cf2992912a08595259674d8425d56e60efa
tracer-ysyx <tracer@ysyx.org>
2025-06-28 15:04:39 +0800

    > compile NEMU ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:04:39 up 6 days, 17:30,  1 user,  load average: 0.30, 0.40, 0.58

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -6,7 +6,7 @@ int main(const char *args);
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
-#define MAINARGS "hhh"
+#define MAINARGS "a little trick"
 #endif
 static const char mainargs[] = MAINARGS;
 
