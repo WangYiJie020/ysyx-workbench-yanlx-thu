21e5aabcc08e7d46b8fa04d63f8ecefbc57f8f19
tracer-ysyx <tracer@ysyx.org>
2025-06-30 16:12:39 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:12:39 up 8 days, 18:38,  1 user,  load average: 0.36, 0.69, 0.76

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -42,7 +42,7 @@ void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
-uint64_t start_time;
+time_t start_time;
 time_t currentTimeABS;
 int flag;
 
