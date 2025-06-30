046ffb5cf2fc9e898d5b30b085b8c950756d52e4
tracer-ysyx <tracer@ysyx.org>
2025-06-30 09:14:51 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:14:51 up 8 days, 11:40,  1 user,  load average: 0.24, 0.53, 0.63

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -20,6 +20,7 @@
 #include "utils.h"
 #include "include.h"
 #include "paddr.h"
+#include <time.h>
 
 
 int cpu_state;
@@ -43,7 +44,10 @@ void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
 extern "C" int pmem_read(int raddr) {
   // 总是读取地址为`raddr & ~0x3u`的4字节返回
-  
+  if(raddr == RTC_ADDR){
+    time_t timep;
+    
+  }
   uint32_t tmp = (uint32_t)raddr /4; //int类型是有符号的，要转成无符号的
   log_write("raddr = %08x,data= %08x\n",raddr,mem[tmp]);
   return mem[tmp];
