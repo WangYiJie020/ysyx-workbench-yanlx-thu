49d43fe058f3cf2c4361d1f6bc517f7f5a2a8d1d
tracer-ysyx <tracer@ysyx.org>
2025-06-30 08:55:28 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:55:28 up 8 days, 11:21,  1 user,  load average: 0.52, 0.54, 0.60

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -54,6 +54,7 @@ extern "C" void pmem_write(int waddr, int wdata, char wmask) {
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
   log_write("                               wmask=%x,waddr = %08x,data= %08x\n",wmask,waddr,wdata);
   if(waddr == RTC_ADDR) {
+    printf("55\n");
     printf("%c",wdata);
     log_write("                               wmask=%x,waddr = %08x,data= %08x\n",wmask,waddr,wdata);
     return;
