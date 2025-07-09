66d463bf2dc3e78b01e216e276b1be1bd5c1ecd4
tracer-ysyx <tracer@ysyx.org>
2025-07-09 11:20:48 +0800

    > compile NEMU ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:20:48 up 1 day,  2:02,  1 user,  load average: 0.60, 0.70, 0.94

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -23,10 +23,22 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     if(ref_r->gpr[i] != cpu.gpr[i]) return false;
   }
   if(ref_r->pc != cpu.pc) return false;
-  if(ref_r->csr_mcause != cpu.csr_mcause) return false;
-  if(ref_r->csr_mepc != cpu.csr_mepc) return false;
-  if(ref_r->csr_mstatus != cpu.csr_mstatus) return false;
-  if(ref_r->csr_mtvec != cpu.csr_mtvec) return false;
+  if(ref_r->csr_mcause != cpu.csr_mcause) {
+    printf("ref_csr_mcause:%x",ref_r->csr_mcause);
+    return false;
+  }
+  if(ref_r->csr_mepc != cpu.csr_mepc) {
+    printf("ref_csr_mepc:%x",ref_r->csr_mepc);
+    return false;
+  }
+  if(ref_r->csr_mstatus != cpu.csr_mstatus) {
+    printf("ref_csr_mstatus:%x",ref_r->csr_mstatus);
+    return false;
+  }
+  if(ref_r->csr_mtvec != cpu.csr_mtvec) {
+    printf("ref_csr_mtvec:%x",ref_r->csr_mtvec);
+    return false;
+  }
   return true;
 }
 
