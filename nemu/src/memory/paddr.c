378a24926cdfc9c159928ac3cda19e1cbf7391c4
tracer-ysyx <tracer@ysyx.org>
2025-07-19 15:02:10 +0800

    > compile NEMU ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:02:10 up 8 days, 18:01,  1 user,  load average: 1.38, 1.67, 1.24

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -71,7 +71,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     pmem_write(addr, len, data); 
     return; 
   }
-  else return;
+  //else return; //so file used to skip
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
