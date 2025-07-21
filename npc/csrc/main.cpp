71b2c7d5394aae6e03862db758bf5508c18189f7
tracer-ysyx <tracer@ysyx.org>
2025-07-21 11:51:37 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:51:37 up 10 days, 14:50,  1 user,  load average: 1.84, 1.25, 1.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -31,7 +31,7 @@ VerilatedContext* contextp = new VerilatedContext;
 Vtop* top = new Vtop{contextp};
 VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
 
-//#define  DIFFTEST_ON
+#define  DIFFTEST_ON
 
 
 enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
