9ca4fd6d2bc9f120b54504254e94e02096949a2c
tracer-ysyx <tracer@ysyx.org>
2025-07-26 11:15:55 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:15:55 up 15 days, 14:15,  1 user,  load average: 2.00, 1.64, 1.49

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -14,7 +14,7 @@ input int csr_reg_1,input int csr_reg_2,input int csr_reg_3);
 module top(
   input clk,
   input rst_n
-  output [31:0]pc,
+  //output [31:0]pc,
   //input [31:0] inst
   //output [31:0] datamem_addr,
   //input [31:0] datamem_data
