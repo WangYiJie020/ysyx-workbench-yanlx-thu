be76056f866f98a23d2005765e642f27c4dc2942
tracer-ysyx <tracer@ysyx.org>
2025-08-01 21:55:15 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:55:15 up 22 days, 54 min,  1 user,  load average: 2.14, 1.06, 1.01

--- npc/vsrc/lsu.v
+++ npc/vsrc/lsu.v
@@ -60,7 +60,7 @@ module lsu(
         .rmask(rmask),
         .read_data(datamem_readdata_o)
     );
-
+/*
     assign lsu_ready_o = lsu_ready_i;
 
     always@(posedge clk) begin
@@ -87,6 +87,76 @@ module lsu(
     always@(posedge clk) begin
         if(!rst_n) lsu_valid_o <= 1'b0;
         else if(lsu_ready_o) lsu_valid_o <= lsu_valid_i;
+    end
+*/
+
+    localparam S_IDLE = 2'b00,S_RECEIVE = 2'b01,S_SEND = 2'b10;
+
+    reg [1:0] current_state,next_state;
+
+    always @(*) begin
+        case(current_state)
+            S_IDLE: begin
+                if (lsu_valid_i == 1 && lsu_ready_o == 1) begin
+                    next_state = S_RECEIVE;
+                end else begin
+                    next_state = current_state;
+                end
+            end
+            
+            S_RECEIVE: begin
+                if (lsu_valid_o == 1 && lsu_ready_i == 1) begin
+                    next_state = S_SEND;  
+                end else begin
+                    next_state = current_state;
+                end
+            end
+            
+            S_SEND: begin
+                next_state = S_IDLE;                 
+            end
+            
+          
+            default: next_state = current_state;
+        endcase
+    end
+
+    always @(posedge clk or negedge rst_n) begin        
+        if (!rst_n) begin
+            current_state <= S_IDLE;
+            lsu_valid_o <= 0;
+            lsu_ready_o <= 0;
+        end else begin
+            current_state <= next_state;
+            if(current_state == S_IDLE) lsu_ready_o <= 1;
+            else if(current_state == S_RECEIVE) lsu_ready_o <= 0;
+            if(current_state == S_RECEIVE) begin 
+                lsu_valid_o <= 1;
+
+                alu_result <= alu_result_i;
+                rs1 <= rs1_i;
+                rs2 <= rs2_i;            
+                MemRead <= MemRead_i;
+                MemWrite <= MemWrite_i;
+                wmask <= wmask_i;
+                rmask <= rmask_i;
+                alu_result <= alu_result_i;
+                wb_src_o <= wb_src_i;
+                csr_write_o <= csr_write_i;
+                csr_wdata_src_o <= csr_wdata_src_i;
+                reg_write_o <= reg_write_i;
+                npc_o <= npc_i;
+                csr_rdata_l_rs1_o <= csr_rdata_l_rs1_i;
+                waddr_o <= waddr_i;
+            end else if (pc == `PC_INIT)begin
+                lsu_valid_o <= 1;
+            end else if (current_state == S_SEND)begin
+                lsu_valid_o <= 1;
+            end else begin
+                lsu_valid_o <= 0;
+            end
+            
+        end
     end
 
 endmodule
