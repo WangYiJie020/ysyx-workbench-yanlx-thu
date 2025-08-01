ce85187ef46474dde53614ae45b08820ae19e54f
tracer-ysyx <tracer@ysyx.org>
2025-08-01 23:39:45 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:39:45 up 22 days,  2:39,  1 user,  load average: 0.53, 0.69, 0.90

--- npc/vsrc/lsu.v
+++ npc/vsrc/lsu.v
@@ -108,17 +108,17 @@ module lsu(
                 if (lsu_valid_o == 1 && lsu_ready_i == 1) begin
                     next_state = S_SEND;  
                 end else begin
-                    next_state = current_state;
+                    next_state = S_WAIT_SEND;
                 end
             end
 
-            //S_WAIT_SEND: begin
-            //    if (lsu_valid_o == 1 && lsu_ready_i == 1) begin
-            //        next_state = S_SEND;  
-            //    end else begin
-            //        next_state = current_state;
-            //    end
-            //end
+            S_WAIT_SEND: begin
+                if (lsu_valid_o == 1 && lsu_ready_i == 1) begin
+                    next_state = S_SEND;  
+                end else begin
+                    next_state = current_state;
+                end
+            end
             
             S_SEND: begin
                 next_state = S_IDLE;                 
@@ -156,10 +156,10 @@ module lsu(
                 npc_o <= npc_i;
                 csr_rdata_l_rs1_o <= csr_rdata_l_rs1_i;
                 waddr_o <= waddr_i;
-            //end else if (current_state == S_WAIT_SEND)begin
-            //    lsu_valid_o <= 1;
-                //MemRead <= 0;
-                //MemWrite <= 0;
+            end else if (current_state == S_WAIT_SEND)begin
+                lsu_valid_o <= 1;
+                MemRead <= 0;
+                MemWrite <= 0;
             end else if (current_state == S_SEND)begin
                 //MemRead <= 0;
                 //MemWrite <= 0;
