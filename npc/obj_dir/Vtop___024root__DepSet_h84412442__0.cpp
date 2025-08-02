719325e058a224fa22cc3fb3f43261bef39b3ee1
tracer-ysyx <tracer@ysyx.org>
2025-08-02 09:37:10 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:37:10 up 22 days, 12:36,  1 user,  load average: 2.17, 1.50, 1.08

--- npc/obj_dir/Vtop___024root__DepSet_h84412442__0.cpp
+++ npc/obj_dir/Vtop___024root__DepSet_h84412442__0.cpp
@@ -150,14 +150,14 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__2(Vtop___024root* vlSelf) 
             vlSelf->top__DOT__waddr_lsu_to_wbu = vlSelf->top__DOT__waddr_exu_to_lsu;
             vlSelf->top__DOT__LSU__DOT__alu_result 
                 = vlSelf->top__DOT__EXU__DOT__alu_result;
-        } else if ((3U != (IData)(vlSelf->top__DOT__LSU__DOT__current_state))) {
-            if ((2U == (IData)(vlSelf->top__DOT__LSU__DOT__current_state))) {
-                vlSelf->top__DOT__valid_lsu_to_wbu = 1U;
-            } else {
-                vlSelf->top__DOT__LSU__DOT__MemRead = 0U;
-                vlSelf->top__DOT__LSU__DOT__MemWrite = 0U;
-                vlSelf->top__DOT__valid_lsu_to_wbu = 0U;
-            }
+        } else if ((3U == (IData)(vlSelf->top__DOT__LSU__DOT__current_state))) {
+            vlSelf->top__DOT__valid_lsu_to_wbu = 1U;
+        } else if ((2U == (IData)(vlSelf->top__DOT__LSU__DOT__current_state))) {
+            vlSelf->top__DOT__valid_lsu_to_wbu = 1U;
+        } else {
+            vlSelf->top__DOT__LSU__DOT__MemRead = 0U;
+            vlSelf->top__DOT__LSU__DOT__MemWrite = 0U;
+            vlSelf->top__DOT__valid_lsu_to_wbu = 0U;
         }
         vlSelf->top__DOT__IFU__DOT__current_state = vlSelf->top__DOT__IFU__DOT__next_state;
     } else {
