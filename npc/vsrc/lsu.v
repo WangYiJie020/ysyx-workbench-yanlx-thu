c65af028e23fee82c70c80d4a562587b5e38ba58
tracer-ysyx <tracer@ysyx.org>
2025-08-02 20:02:28 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:02:28 up  3:27,  1 user,  load average: 1.51, 1.05, 1.05

--- npc/vsrc/lsu.v
+++ npc/vsrc/lsu.v
@@ -108,7 +108,7 @@ module lsu(
                 if (lsu_valid_o == 1 && lsu_ready_i == 1) begin
                     next_state = S_SEND;  
                 end else begin
-                    next_state = S_SEND;
+                    next_state = current_state;
                 end
             end
 
@@ -160,7 +160,7 @@ module lsu(
             //    lsu_valid_o <= 1;
                 //MemWrite <= 0;
             end else if (current_state == S_SEND)begin
-                MemRead <= 0;
+                //MemRead <= 0;
                 //MemWrite <= 0;
                 lsu_valid_o <= 1;
             end else begin
