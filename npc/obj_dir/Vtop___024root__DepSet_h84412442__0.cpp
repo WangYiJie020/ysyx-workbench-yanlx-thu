aa88f94a99ac46c28fc0c4d04ac8e744e87d38e9
tracer-ysyx <tracer@ysyx.org>
2025-08-06 00:15:42 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:15:42 up 13:03,  1 user,  load average: 0.88, 0.83, 0.75

--- npc/obj_dir/Vtop___024root__DepSet_h84412442__0.cpp
+++ npc/obj_dir/Vtop___024root__DepSet_h84412442__0.cpp
@@ -580,6 +580,10 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__2(Vtop___024root* vlSelf) 
                                                       | (((IData)(vlSelf->top__DOT__lsu_bready) 
                                                           & (IData)(vlSelf->top__DOT__lsu_bvalid)) 
                                                          & (~ (IData)(vlSelf->top__DOT__lsu_bresp))))));
+            if ((1U & (~ (IData)(vlSelf->top__DOT__lsu_bresp)))) {
+                vlSelf->top__DOT__lsu_wvalid = 0U;
+                vlSelf->top__DOT__lsu_awvalid = 0U;
+            }
         } else if ((2U == (IData)(vlSelf->top__DOT__LSU__DOT__current_state))) {
             vlSelf->top__DOT__ready_exu_to_lsu = 1U;
             vlSelf->top__DOT__valid_lsu_to_wbu = 0U;
