b36e83b5ad577e542686c0847c03a1f569fe3f4e
tracer-ysyx <tracer@ysyx.org>
2025-09-06 19:07:27 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:07:27 up 10 days,  8:29,  1 user,  load average: 0.96, 0.80, 0.75

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -50,7 +50,8 @@ int flag = 0;
 extern "C" void flash_read(int32_t addr, int32_t *data) { assert(0); }
 extern "C" void mrom_read(int32_t addr, int32_t *data) { 
   uint32_t tmp = (uint32_t)addr / 4;
-  *data = mem[tmp]; 
+  //*data = mem[tmp]; 
+  *data = 0x00100073;
 }
 
 
