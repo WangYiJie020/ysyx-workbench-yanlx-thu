832e4d1750c6fef92b04e86bec20c98e316ee751
tracer-ysyx <tracer@ysyx.org>
2025-09-06 18:50:00 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:50:00 up 10 days,  8:11,  1 user,  load average: 1.08, 0.76, 0.70

--- npc/char-test.c
+++ npc/char-test.c
@@ -1,6 +1,6 @@
 #define UART_BASE 0x10000000L
 #define UART_TX   0
-void main() {
+void _start() {
   *(volatile char *)(UART_BASE + UART_TX) = 'A';
   *(volatile char *)(UART_BASE + UART_TX) = '\n';
   while (1);
