5c0f7d89d188dfbbaf9da2f479601d5c4dda7bcd
tracer-ysyx <tracer@ysyx.org>
2025-09-12 19:30:25 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:30:25 up 5 days, 19:41,  1 user,  load average: 1.57, 1.56, 1.54

--- npc/obj_dir/VysyxSoCFull__Trace__0__Slow.cpp
+++ npc/obj_dir/VysyxSoCFull__Trace__0__Slow.cpp
@@ -5653,7 +5653,7 @@ VL_ATTR_COLD void VysyxSoCFull___024root__trace_full_sub_0(VysyxSoCFull___024roo
     bufp->fullIData(oldp+1382,(vlSelf->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__IFU__DOT__rready_buffer),32);
     bufp->fullCData(oldp+1383,(3U),3);
     bufp->fullCData(oldp+1384,(4U),3);
-    bufp->fullIData(oldp+1385,(0x20000008U),32);
+    bufp->fullIData(oldp+1385,(0x20000000U),32);
     bufp->fullIData(oldp+1386,(vlSelf->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__LSU__DOT__rs2),32);
     bufp->fullIData(oldp+1387,(vlSelf->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__LSU__DOT__csr_rdata_l_rs1),32);
     bufp->fullBit(oldp+1388,(vlSelf->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__LSU__DOT__wvalid_tmp));
