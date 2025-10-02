1fbac389a88b54c124a103b46ba7226d895b639e
tracer-ysyx <tracer@ysyx.org>
2025-10-02 21:10:24 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:10:24 up 7 days,  9:01,  1 user,  load average: 0.80, 1.14, 1.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -250,7 +250,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
       "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
 
   ref_difftest_init();
-  ref_difftest_memcpy(PMEM_LEFT, mem+0x20000000, img_size, DIFFTEST_TO_REF);
+  ref_difftest_memcpy(PMEM_LEFT, mem+0x8000000, img_size, DIFFTEST_TO_REF);
   cpu.pc = 0x20000000;
   cpu.csr_mstatus = 0x1800;
   cpu.csr_mcause = 0;
