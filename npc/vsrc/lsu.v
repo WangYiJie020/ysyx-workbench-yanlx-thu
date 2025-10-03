c19b8c7604c2f9ec417b520a2ecfc7d1f64a5228
tracer-ysyx <tracer@ysyx.org>
2025-10-03 22:40:13 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:40:13 up 8 days, 10:31,  1 user,  load average: 1.31, 0.70, 0.59

--- npc/vsrc/lsu.v
+++ npc/vsrc/lsu.v
@@ -116,31 +116,31 @@ module lsu(
     //assign datamem_readdata_o = rdata_i;
     assign arid_o = 0;
     assign arlen_o = 0;
-    assign arsize_o = 3'b010;
+    //assign arsize_o = 3'b010;
     assign arburst_o = 0;
     assign awid_o = 0;
     assign awlen_o = 0;
-    assign awsize_o = 3'b010;
+    //assign awsize_o = 3'b010;
     assign awburst_o = 0;
     assign wlast_o = 1;
 
-    /*always@(*) begin
+    always@(*) begin
         case(wmask_i)
             4'h1: awsize_o = 3'b000;
             4'h3: awsize_o = 3'b001;
             4'hf: awsize_o = 3'b010;
             default: awsize_o = 3'b010;
         endcase
-    end*/
+    end
 
-    /*always@(*) begin
+    always@(*) begin
         case(rmask_i)
             3'b100,3'b011: arsize_o = 3'b000; //lb,lbu
             3'b010,3'b001: arsize_o = 3'b001; //lh,lhu
             3'b000: arsize_o = 3'b010; //lw
             default: arsize_o = 3'b010;
         endcase
-    end*/
+    end
 
     assign wstrb = wmask_i;
 
