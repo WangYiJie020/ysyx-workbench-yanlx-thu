baea464c7c7de68e0e8ce314bf2463fcc5767801
tracer-ysyx <tracer@ysyx.org>
2025-10-28 00:08:34 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:08:34 up 6 days,  1:52,  1 user,  load average: 1.47, 1.74, 1.84

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -33,7 +33,7 @@ VerilatedContext* contextp = new VerilatedContext;
 VysyxSoCFull* top = new VysyxSoCFull{contextp};
 VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
 
-#define  DIFFTEST_ON
+//#define  DIFFTEST_ON
 #define  WAVE_ON
 #define  TRACE_ON
 
