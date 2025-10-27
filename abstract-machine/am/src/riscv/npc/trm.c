50c3d21cfc654976501d92652e2b53d379c40da7
tracer-ysyx <tracer@ysyx.org>
2025-10-28 01:13:40 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:13:40 up 6 days,  2:57,  1 user,  load average: 1.94, 2.29, 2.13

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -4,6 +4,7 @@
 
 
 extern char _heap_start;
+extern char _heap_end;
 int main(const char *args);
 
 # define DEVICE_BASE 0xa0000000
@@ -18,10 +19,9 @@ static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)a
 
 extern char _pmem_start;
 #define PMEM_SIZE (128 * 1024 * 1024)
-//#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
-#define PMEM_END  ((uintptr_t)&_heap_start + PMEM_SIZE)
+#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
-Area heap = RANGE(&_heap_start, PMEM_END);
+Area heap = RANGE(&_heap_start, &_heap_end);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
