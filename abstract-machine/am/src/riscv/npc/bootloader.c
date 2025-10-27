0e87c36b9dda5824247509613aac5b40ea767b14
tracer-ysyx <tracer@ysyx.org>
2025-10-28 01:41:22 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:41:22 up 6 days,  3:24,  1 user,  load average: 2.12, 2.05, 2.02

--- abstract-machine/am/src/riscv/npc/bootloader.c
+++ abstract-machine/am/src/riscv/npc/bootloader.c
@@ -6,18 +6,23 @@ extern uint8_t _data_lma_start[];
 extern uint8_t _data_vma_start[];
 extern uint8_t _data_vma_end[];
 extern uint8_t _trm_init[];
+extern uint8_t _pmem_start[];
+extern uint8_t _sidata[];
+
 
 void __attribute__((section(".bootloader"))) _bootloader_init() {
     size_t data_size = (size_t)(_data_vma_end - _data_vma_start);
+    uint8_t *d = (uint8_t *)_data_vma_start;
+    uint8_t *s = (uint8_t *)_data_lma_start;
     if (data_size > 0) {
-        //memcpy(_data_vma_start, _data_lma_start, data_size);
-        uint8_t *d = (uint8_t *)_data_vma_start;
-        uint8_t *s = (uint8_t *)_data_lma_start;
+        //memcpy(_data_vma_start, _data_lma_start, data_size);        
         while (data_size--) {
             *d = *s;
             d++;
             s++;
         }
     }
+    //size_t code_size = (size_t)(_sidata - _pmem_start);
+
     asm volatile ("jal %0" : : "i"(_trm_init));
 }
