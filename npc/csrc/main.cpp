828f7443089ae08e9838d81b3322c285deba0cae
tracer-ysyx <tracer@ysyx.org>
2025-10-30 12:43:18 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-86-generic #87~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Sep 29 09:48:07 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:43:18 up  3:27,  1 user,  load average: 1.71, 1.42, 1.64

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -35,7 +35,7 @@ VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
 
 //#define  DIFFTEST_ON
 //#define  WAVE_ON
-//#define  TRACE_ON
+#define  TRACE_ON
 
 
 enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
