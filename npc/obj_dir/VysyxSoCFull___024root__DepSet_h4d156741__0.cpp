8595c2f9be6106ebf68e5913739d707722cecb3a
tracer-ysyx <tracer@ysyx.org>
2025-11-26 15:28:24 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:28:24 up 6 days, 15:29,  1 user,  load average: 2.22, 2.05, 1.74

--- npc/obj_dir/VysyxSoCFull___024root__DepSet_h4d156741__0.cpp
+++ npc/obj_dir/VysyxSoCFull___024root__DepSet_h4d156741__0.cpp
@@ -4551,6 +4551,7 @@ VL_INLINE_OPT void VysyxSoCFull___024root___nba_sequent__TOP__8(VysyxSoCFull___0
                     vlSelf->ysyxSoCFull__DOT__sdram__DOT__current_column 
                         = (0x3ffU & (IData)(vlSelf->ysyxSoCFull__DOT__asic__DOT__lsdram_apb__DOT__msdram__DOT__u_sdram_ctrl__DOT__addr_q));
                     vlSelf->ysyxSoCFull__DOT__sdram__DOT__read_flag = 1U;
+                    vlSelf->ysyxSoCFull__DOT__sdram__DOT__flag = 0U;
                     vlSelf->ysyxSoCFull__DOT__sdram__DOT__CAS_counter 
                         = (7U & ((IData)(vlSelf->ysyxSoCFull__DOT__sdram__DOT__CAS_latency) 
                                  - (IData)(2U)));
@@ -4566,7 +4567,6 @@ VL_INLINE_OPT void VysyxSoCFull___024root___nba_sequent__TOP__8(VysyxSoCFull___0
                     vlSelf->ysyxSoCFull__DOT__sdram__DOT__current_column 
                         = (0x3ffU & (IData)(vlSelf->ysyxSoCFull__DOT__asic__DOT__lsdram_apb__DOT__msdram__DOT__u_sdram_ctrl__DOT__addr_q));
                     vlSelf->ysyxSoCFull__DOT__sdram__DOT__write_flag = 1U;
-                    vlSelf->ysyxSoCFull__DOT__sdram__DOT__flag = 0U;
                     __Vdly__ysyxSoCFull__DOT__sdram__DOT__burst_counter 
                         = (7U & ((IData)(vlSelf->ysyxSoCFull__DOT__sdram__DOT__burst_length) 
                                  - (IData)(1U)));
