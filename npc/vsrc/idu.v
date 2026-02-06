d3cd4754d13d9449e937fb30ad4d360e9251b093
tracer-ysyx <tracer@ysyx.org>
2026-02-06 18:59:02 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:59:02 up 26 days,  1:37,  1 user,  load average: 0.71, 0.33, 0.17

--- npc/vsrc/idu.v
+++ npc/vsrc/idu.v
@@ -196,15 +196,16 @@ module idu(
                 //end
                 //else 
                 //if(flag==1 && wbu_submit) begin 
-                idu_valid_o <= 0;
+                
                // end
-                
-                
-            end else if (current_state == S_SEND)begin
                 if(isRAW ) begin//current_state <= S_RECEIVE;
                     idu_valid_o <= 0;
                 end
                 else idu_valid_o <= 1;
+                
+                
+            end else if (current_state == S_SEND)begin
+                idu_valid_o <= 0;
                 idu_counter_return({1'b0,inst[6:0]});
                 
             end else begin
