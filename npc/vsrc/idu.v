a1a499e0bbbf3032b833227b127f6befd4ea3e18
tracer-ysyx <tracer@ysyx.org>
2026-02-25 15:40:27 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:40:27 up 7 days,  4:27,  1 user,  load average: 1.02, 1.00, 0.93

--- npc/vsrc/idu.v
+++ npc/vsrc/idu.v
@@ -92,7 +92,6 @@ module idu(
     assign wbu_raw = (opcode_r | opcode_s | opcode_sb) ? (((raddr1 == wbu_rd && raddr1 != 0) || (raddr2 == wbu_rd && raddr2 != 0))? 1 : 0) :
                                     ((opcode_u || opcode_uj)? 0 : ((raddr1 == wbu_rd && raddr1 != 0)? 1 : 0));
 
-    assign 
     assign isRAW = ((exu_raw) && (current_state == S_RECEIVE)) |
                    ((lsu_raw) && (current_state == S_RECEIVE)) |
                    ((wbu_raw) && (current_state == S_RECEIVE));
