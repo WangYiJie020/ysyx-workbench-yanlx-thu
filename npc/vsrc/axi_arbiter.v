6144c1bbc2b060ed72e356ee7c0db92fd3f04797
tracer-ysyx <tracer@ysyx.org>
2026-03-02 16:30:51 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:30:51 up 12 days,  5:18,  1 user,  load average: 1.67, 1.75, 1.62

--- npc/vsrc/axi_arbiter.v
+++ npc/vsrc/axi_arbiter.v
@@ -117,7 +117,9 @@ module axi_arbiter (
     input  [1:0]            bresp_i,
     input  [3:0]            bid_i,
     input                   bvalid_i,
-    output                  bready_o
+    output                  bready_o,
+
+    output bus_busy
 );
 
 // ============================================================
@@ -230,7 +232,18 @@ always @(*) begin
         rvalid_o_b = rvalid_i;
     end
 end
+    always@(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            bus_busy <= 0;
+        end
+        else begin
+            if(arready_i==1 && arvalid_o==1)
+                bus_busy <= 1;
+            if(rvalid_i==1 && rready_o==1 && rlast_i==1)
+                bus_busy <= 0;
 
+        end
+    end
     assign awaddr_o = awaddr_i_b;
     assign awid_o = awid_i_b;
     assign awlen_o = awlen_i_b;
