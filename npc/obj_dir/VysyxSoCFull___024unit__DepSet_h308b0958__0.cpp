178bfdc2924afc1508b26257bd8b180d11450062
tracer-ysyx <tracer@ysyx.org>
2026-03-30 15:50:22 +0800

    > sim RTL ysyx_25050137 闫凌霄 Linux yanlx-ThinkPad-E15 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:50:22 up  1:50,  1 user,  load average: 0.94, 0.93, 0.95

--- npc/obj_dir/VysyxSoCFull___024unit__DepSet_h308b0958__0.cpp
+++ npc/obj_dir/VysyxSoCFull___024unit__DepSet_h308b0958__0.cpp
@@ -26,9 +26,9 @@ VL_INLINE_OPT void VysyxSoCFull___024unit____Vdpiimwrap_difftest_next_step_TOP__
     difftest_next_step(difftest_check__Vcvt);
 }
 
-extern "C" void reg_return_value(int gpr_0, int gpr_1, int gpr_2, int gpr_3, int gpr_4, int gpr_5, int gpr_6, int gpr_7, int gpr_8, int gpr_9, int gpr_10, int gpr_11, int gpr_12, int gpr_13, int gpr_14, int gpr_15, int gpr_16, int gpr_17, int gpr_18, int gpr_19, int gpr_20, int gpr_21, int gpr_22, int gpr_23, int gpr_24, int gpr_25, int gpr_26, int gpr_27, int gpr_28, int gpr_29, int gpr_30, int gpr_31, int pc, int csr_reg_0, int csr_reg_1, int csr_reg_2, int csr_reg_3);
+extern "C" void reg_return_value(int gpr_0, int gpr_1, int gpr_2, int gpr_3, int gpr_4, int gpr_5, int gpr_6, int gpr_7, int gpr_8, int gpr_9, int gpr_10, int gpr_11, int gpr_12, int gpr_13, int gpr_14, int gpr_15, int pc, int csr_reg_0, int csr_reg_1, int csr_reg_2, int csr_reg_3);
 
-VL_INLINE_OPT void VysyxSoCFull___024unit____Vdpiimwrap_reg_return_value_TOP____024unit(IData/*31:0*/ gpr_0, IData/*31:0*/ gpr_1, IData/*31:0*/ gpr_2, IData/*31:0*/ gpr_3, IData/*31:0*/ gpr_4, IData/*31:0*/ gpr_5, IData/*31:0*/ gpr_6, IData/*31:0*/ gpr_7, IData/*31:0*/ gpr_8, IData/*31:0*/ gpr_9, IData/*31:0*/ gpr_10, IData/*31:0*/ gpr_11, IData/*31:0*/ gpr_12, IData/*31:0*/ gpr_13, IData/*31:0*/ gpr_14, IData/*31:0*/ gpr_15, IData/*31:0*/ gpr_16, IData/*31:0*/ gpr_17, IData/*31:0*/ gpr_18, IData/*31:0*/ gpr_19, IData/*31:0*/ gpr_20, IData/*31:0*/ gpr_21, IData/*31:0*/ gpr_22, IData/*31:0*/ gpr_23, IData/*31:0*/ gpr_24, IData/*31:0*/ gpr_25, IData/*31:0*/ gpr_26, IData/*31:0*/ gpr_27, IData/*31:0*/ gpr_28, IData/*31:0*/ gpr_29, IData/*31:0*/ gpr_30, IData/*31:0*/ gpr_31, IData/*31:0*/ pc, IData/*31:0*/ csr_reg_0, IData/*31:0*/ csr_reg_1, IData/*31:0*/ csr_reg_2, IData/*31:0*/ csr_reg_3) {
+VL_INLINE_OPT void VysyxSoCFull___024unit____Vdpiimwrap_reg_return_value_TOP____024unit(IData/*31:0*/ gpr_0, IData/*31:0*/ gpr_1, IData/*31:0*/ gpr_2, IData/*31:0*/ gpr_3, IData/*31:0*/ gpr_4, IData/*31:0*/ gpr_5, IData/*31:0*/ gpr_6, IData/*31:0*/ gpr_7, IData/*31:0*/ gpr_8, IData/*31:0*/ gpr_9, IData/*31:0*/ gpr_10, IData/*31:0*/ gpr_11, IData/*31:0*/ gpr_12, IData/*31:0*/ gpr_13, IData/*31:0*/ gpr_14, IData/*31:0*/ gpr_15, IData/*31:0*/ pc, IData/*31:0*/ csr_reg_0, IData/*31:0*/ csr_reg_1, IData/*31:0*/ csr_reg_2, IData/*31:0*/ csr_reg_3) {
     VL_DEBUG_IF(VL_DBG_MSGF("+        VysyxSoCFull___024unit____Vdpiimwrap_reg_return_value_TOP____024unit\n"); );
     // Body
     int gpr_0__Vcvt;
@@ -63,38 +63,6 @@ VL_INLINE_OPT void VysyxSoCFull___024unit____Vdpiimwrap_reg_return_value_TOP____
     for (size_t gpr_14__Vidx = 0; gpr_14__Vidx < 1; ++gpr_14__Vidx) gpr_14__Vcvt = gpr_14;
     int gpr_15__Vcvt;
     for (size_t gpr_15__Vidx = 0; gpr_15__Vidx < 1; ++gpr_15__Vidx) gpr_15__Vcvt = gpr_15;
-    int gpr_16__Vcvt;
-    for (size_t gpr_16__Vidx = 0; gpr_16__Vidx < 1; ++gpr_16__Vidx) gpr_16__Vcvt = gpr_16;
-    int gpr_17__Vcvt;
-    for (size_t gpr_17__Vidx = 0; gpr_17__Vidx < 1; ++gpr_17__Vidx) gpr_17__Vcvt = gpr_17;
-    int gpr_18__Vcvt;
-    for (size_t gpr_18__Vidx = 0; gpr_18__Vidx < 1; ++gpr_18__Vidx) gpr_18__Vcvt = gpr_18;
-    int gpr_19__Vcvt;
-    for (size_t gpr_19__Vidx = 0; gpr_19__Vidx < 1; ++gpr_19__Vidx) gpr_19__Vcvt = gpr_19;
-    int gpr_20__Vcvt;
-    for (size_t gpr_20__Vidx = 0; gpr_20__Vidx < 1; ++gpr_20__Vidx) gpr_20__Vcvt = gpr_20;
-    int gpr_21__Vcvt;
-    for (size_t gpr_21__Vidx = 0; gpr_21__Vidx < 1; ++gpr_21__Vidx) gpr_21__Vcvt = gpr_21;
-    int gpr_22__Vcvt;
-    for (size_t gpr_22__Vidx = 0; gpr_22__Vidx < 1; ++gpr_22__Vidx) gpr_22__Vcvt = gpr_22;
-    int gpr_23__Vcvt;
-    for (size_t gpr_23__Vidx = 0; gpr_23__Vidx < 1; ++gpr_23__Vidx) gpr_23__Vcvt = gpr_23;
-    int gpr_24__Vcvt;
-    for (size_t gpr_24__Vidx = 0; gpr_24__Vidx < 1; ++gpr_24__Vidx) gpr_24__Vcvt = gpr_24;
-    int gpr_25__Vcvt;
-    for (size_t gpr_25__Vidx = 0; gpr_25__Vidx < 1; ++gpr_25__Vidx) gpr_25__Vcvt = gpr_25;
-    int gpr_26__Vcvt;
-    for (size_t gpr_26__Vidx = 0; gpr_26__Vidx < 1; ++gpr_26__Vidx) gpr_26__Vcvt = gpr_26;
-    int gpr_27__Vcvt;
-    for (size_t gpr_27__Vidx = 0; gpr_27__Vidx < 1; ++gpr_27__Vidx) gpr_27__Vcvt = gpr_27;
-    int gpr_28__Vcvt;
-    for (size_t gpr_28__Vidx = 0; gpr_28__Vidx < 1; ++gpr_28__Vidx) gpr_28__Vcvt = gpr_28;
-    int gpr_29__Vcvt;
-    for (size_t gpr_29__Vidx = 0; gpr_29__Vidx < 1; ++gpr_29__Vidx) gpr_29__Vcvt = gpr_29;
-    int gpr_30__Vcvt;
-    for (size_t gpr_30__Vidx = 0; gpr_30__Vidx < 1; ++gpr_30__Vidx) gpr_30__Vcvt = gpr_30;
-    int gpr_31__Vcvt;
-    for (size_t gpr_31__Vidx = 0; gpr_31__Vidx < 1; ++gpr_31__Vidx) gpr_31__Vcvt = gpr_31;
     int pc__Vcvt;
     for (size_t pc__Vidx = 0; pc__Vidx < 1; ++pc__Vidx) pc__Vcvt = pc;
     int csr_reg_0__Vcvt;
@@ -105,7 +73,7 @@ VL_INLINE_OPT void VysyxSoCFull___024unit____Vdpiimwrap_reg_return_value_TOP____
     for (size_t csr_reg_2__Vidx = 0; csr_reg_2__Vidx < 1; ++csr_reg_2__Vidx) csr_reg_2__Vcvt = csr_reg_2;
     int csr_reg_3__Vcvt;
     for (size_t csr_reg_3__Vidx = 0; csr_reg_3__Vidx < 1; ++csr_reg_3__Vidx) csr_reg_3__Vcvt = csr_reg_3;
-    reg_return_value(gpr_0__Vcvt, gpr_1__Vcvt, gpr_2__Vcvt, gpr_3__Vcvt, gpr_4__Vcvt, gpr_5__Vcvt, gpr_6__Vcvt, gpr_7__Vcvt, gpr_8__Vcvt, gpr_9__Vcvt, gpr_10__Vcvt, gpr_11__Vcvt, gpr_12__Vcvt, gpr_13__Vcvt, gpr_14__Vcvt, gpr_15__Vcvt, gpr_16__Vcvt, gpr_17__Vcvt, gpr_18__Vcvt, gpr_19__Vcvt, gpr_20__Vcvt, gpr_21__Vcvt, gpr_22__Vcvt, gpr_23__Vcvt, gpr_24__Vcvt, gpr_25__Vcvt, gpr_26__Vcvt, gpr_27__Vcvt, gpr_28__Vcvt, gpr_29__Vcvt, gpr_30__Vcvt, gpr_31__Vcvt, pc__Vcvt, csr_reg_0__Vcvt, csr_reg_1__Vcvt, csr_reg_2__Vcvt, csr_reg_3__Vcvt);
+    reg_return_value(gpr_0__Vcvt, gpr_1__Vcvt, gpr_2__Vcvt, gpr_3__Vcvt, gpr_4__Vcvt, gpr_5__Vcvt, gpr_6__Vcvt, gpr_7__Vcvt, gpr_8__Vcvt, gpr_9__Vcvt, gpr_10__Vcvt, gpr_11__Vcvt, gpr_12__Vcvt, gpr_13__Vcvt, gpr_14__Vcvt, gpr_15__Vcvt, pc__Vcvt, csr_reg_0__Vcvt, csr_reg_1__Vcvt, csr_reg_2__Vcvt, csr_reg_3__Vcvt);
 }
 
 extern "C" void flash_read(int addr, int* data);
